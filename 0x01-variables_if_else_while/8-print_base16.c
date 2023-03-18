ca36e014805315e5c8c8ba441381d73ee1f52122
mamesareagan <mamesareagan0@gmail.com>
2023-03-17 17:58:16 -0700

    base16 digits

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n = 48;
+	int a = 97;
+
+	while (n <= 57)
+	{
+		putchar(n);
+		n++;
+	}
+	while (a <= 102)
+	{
+		putchar(a);
+		a++;
+	}
+	putchar('\n');
+	return (0);
+}
