65561058054875cbc09b03d675a6c7edb40a96d3
mamesareagan <mamesareagan0@gmail.com>
2023-03-19 00:54:38 -0700

    print two numbers ,not equal

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,15 +8,15 @@ int main(void)
 {
 	int n1 = 48;
 	int n2;
-	
+
 	for (n1; n1 <= 56; n1++)
 	{
-		for (n2 = n1+1; n2 <= 57; n2++)
+		for (n2 = n1 + 1; n2 <= 57; n2++)
 		{
 			putchar(n1);
 			putchar(n2);
 			if (n1 == 56 && n2 == 57)
-				continue;	
+				continue;
 			putchar(',');
 			putchar(' ');
 		}
