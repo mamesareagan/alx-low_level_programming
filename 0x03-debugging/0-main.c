79da76894f45eec938e28ae6ee7f2f033f34fd86
mamesareagan <mamesareagan0@gmail.com>
2023-03-23 21:17:39 -0700

    checks code

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * main - checks a code
- * return: 0
+ * Return: 0
  */
 int main(void)
 {
@@ -9,6 +9,5 @@ int main(void)
 
 	i = 0;
 	positive_or_negative(i);
-	
 	return (0);
 }
