0798c3ae5546d5db9b944225d76c7c7d3d2f2dee
mamesareagan <mamesareagan0@gmail.com>
2023-04-03 19:16:54 -0700

    strspn

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * _strspn - gets length of a prefix substring
+ * @s - string to be checked
+ * @accept: string containing accepted characters
+ *
+ * Return: the number of bytes in the initial segment of s which consist
+ * only of bytes from accept.
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int b = 0;
+	int i;
+
+	while (*s)
+	{
+		for (i = 0; accept[i]; i++)
+		{
+			if (*s == accept[i])
+			{
+				b++;
+				break;
+			}
+
+			else if (accept[i + 1] == '\0')
+				return (b);
+		}
+
+		s++;
+	}
+
+	return (b);
+}
