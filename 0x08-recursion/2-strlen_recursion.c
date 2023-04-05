7ab9b583527a7ca4e0840d298b5fe702dd1e8faf
mamesareagan <mamesareagan0@gmail.com>
2023-04-05 04:39:45 -0700

    string length

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _strlen_recursion - Gets lenghh of a string
+ * @s:String to be counted 
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')/*checks if character pointed by s is NULL*/
+	{
+		return (0);
+	}
+	else 
+	{
+		return (1 + _strlen_recursion(s + 1));
+	}
+}
