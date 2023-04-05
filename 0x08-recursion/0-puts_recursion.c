216ebaafd153e6c5d6e58ebc7c3b3c51197a6f6c
mamesareagan <mamesareagan0@gmail.com>
2023-04-05 01:59:36 -0700

    prints a string, followed by a new line

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _puts_recursion - prints a string followed by new line
+ * @s: String to be printed
+ */
+void _puts_recursion(char *s)
+{
+	/*print first address of s*/
+	_putchar(*s);
+	/*Ending the recursion*/
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	++s;/*move to next address of s*/
+	_puts_recursion(s);/*recalled function*/
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,4 +1,4 @@
-#ifndef MAIN.H
+#ifndef MAIN
 #define MAIN
 
 int _putchar(char c);
