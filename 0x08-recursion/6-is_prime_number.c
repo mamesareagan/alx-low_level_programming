2ac63cedb40d75550118892a76ef7fa629edfb1d
mamesareagan <mamesareagan0@gmail.com>
2023-04-06 11:55:21 -0700

    checks for a prime number

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,39 @@
+#include "main"
+/**
+ * is_divisible - Checks if a number is divisible.
+ * @n: The number to be checked.
+ * @v: The divisor.
+ *
+ * Return: If the number is divisible - 0.
+ *         If the number is not divisible - 1.
+ */
+int divisible(int n, int v)
+{
+	if (n % v == 0)
+		return (0);
+
+	if (v == n / 2)
+		return (1);
+
+	return (divisible(n, v + 1));
+}
+
+/**
+ * is_prime_number - Checks if a number is prime.
+ * @n: The number to be checked.
+ *
+ * Return: If the integer is not prime - 0.
+ *         If the number is prime - 1.
+ */
+int is_prime_number(int n)
+{
+	int v = 2;
+
+	if (n <= 1)
+		return (0);
+
+	if (n >= 2 && n <= 3)
+		return (1);
+
+	return (divisible(n, v));
+}
