bceed688781c57a54d7eeccb5989a32fa465c22c
mamesareagan <mamesareagan0@gmail.com>
2023-04-13 19:41:40 -0700

    reallocates a memory block using malloc and free

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,20 +1,55 @@
 #include "main.h"
 #include <stdlib.h>
 
+/**
+ * _realloc - Reallocates a memory block using malloc and free.
+ * @ptr: A pointer to the memory previously allocated.
+ * @old_size: The size in bytes of the allocated space for ptr.
+ * @new_size: The size in bytes for the new memory block.
+ *
+ * Return: If new_size == old_size - ptr.
+ *         If new_size == 0 and ptr is not NULL - NULL.
+ *         Otherwise - a pointer to the reallocated memory block.
+ */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	if (newsize == 0 && ptr != NULL)
+	void *m;
+	char *ptr_copy, *r;
+	unsigned int index;
+
+	if (new_size == old_size)
+		return (ptr);
+
+	if (ptr == NULL)
+	{
+		m = malloc(new_size);
+
+		if (m == NULL)
+			return (NULL);
+
+		return (m);
+	}
+
+	if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
 		return (NULL);
 	}
-	if (ptr == NULL)
+
+	ptr_copy = ptr;
+	m = malloc(sizeof(*ptr_copy) * new_size);
+
+	if (m == NULL)
 	{
-		ptr = malloc(new_size);
+		free(ptr);
+		return (NULL);
 	}
-	if (new_size == old_size)
-	{
-		return (ptr);
-	}
-	return (ptr);
+
+	r = m;
+
+	for (index = 0; index < old_size && index < new_size; index++)
+		r[index] = *ptr_copy++;
+
+	free(ptr);
+	return (m);
 }
