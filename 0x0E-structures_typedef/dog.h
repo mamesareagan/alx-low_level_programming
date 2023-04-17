baec1abca06dcb5d916254515594d5ac9d871dd3
mamesareagan <mamesareagan0@gmail.com>
2023-04-17 00:49:49 -0700

    new type of our structure

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,11 @@
 #ifndef DOG_H
 #define DOG_H
 
+typedef struct dog dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 /**
  * struct dog - has dogname,age and owner
  * @name: First member
