891592b5a441f5fbdafb800623c8ca5eb1cd5cd4
mamesareagan <mamesareagan0@gmail.com>
2023-04-17 09:10:13 -0700

    initialize structure

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+/**
+ * init_dog - initializes variables of struct dog
+ * @d:structure variable
+ * @name:name of dog
+ * @age:Dog's age
+ * @owner:dogs owner
+ *
+ * Return: nothing
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
