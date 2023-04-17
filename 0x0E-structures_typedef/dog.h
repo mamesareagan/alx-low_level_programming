555dabfdf38f6a776fad44e56396953a7d2c5ed7
mamesareagan <mamesareagan0@gmail.com>
2023-04-17 00:16:06 -0700

    described a structure

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - has dogname,age and owner
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: all about dogs
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+#endif /* DOG_H*/
+
